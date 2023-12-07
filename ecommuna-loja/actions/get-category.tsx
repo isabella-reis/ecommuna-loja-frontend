6d7909a812df27aa9346df947d2ba4015f474cf5
Isabella Reis <isabellanery15@hotmail.com>
2023-12-07 20:41:35 -0300

    bugfix - categorias

--- ecommuna-loja/actions/get-category.tsx
+++ ecommuna-loja/actions/get-category.tsx
@@ -2,8 +2,8 @@ import { Category } from "@/types";
 
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategory = async (id: string): Promise<Category> => {
-  const res = await fetch(`${URL}/${id}`);
+const getCategory = async (): Promise<Category[]> => {
+  const res = await fetch(URL);
 
   return res.json();
 };
